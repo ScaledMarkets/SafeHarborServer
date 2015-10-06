e0a30c30ed286443d5b336b1d41ba0ab730c3b90
Clifford Berg <cliff@cliffberg.com>
2015-10-06 19:40:35 -0400

    Added error response to ServeHTTP

--- src/main/Server.go
+++ src/main/Server.go
@@ -267,7 +267,7 @@ func (server *Server) ServeHTTP(writer http.ResponseWriter, httpReq *http.Reques
 	sessionToken = server.authService.authenticateRequest(httpReq)
 	if sessionToken == nil { //return authent failure
 		fmt.Println("Failed to authenticate - request being denied")
-		writer.WriteHeader(401)
+		respondWithClientError(writer, "Failed to authenticate - request being denied")
 		return
 	}
 	fmt.Println("authenticated request")
