c80798c74ee1d05defa59a86febfd1631481f6fb
Clifford Berg <cliff@cliffberg.com>
2015-10-18 20:37:59 -0400

    Adding run.sh.

--- build/Centos/run.sh
+++ build/Centos/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run SafeHarborServer, using the conf.json file in the current directory.
 
-sudo nohup ./SafeHarborServer --debug > log.out 2> log.err < /dev/null &
+sudo nohup ./SafeHarborServer -debug > log.out 2> log.err < /dev/null &
