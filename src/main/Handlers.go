c920c01603597362eaa6027cd3aa85c2b1ae505d
Clifford Berg <cliff@cliffberg.com>
2015-11-11 20:27:26 -0500

    Removed chdir from execDockerfile.

--- src/main/Handlers.go
+++ src/main/Handlers.go
@@ -943,7 +943,7 @@ func execDockerfile(server *Server, sessionToken *SessionToken, values url.Value
 	// docker.io/cesanta/docker_auth   latest              3d31749deac5        3 months ago        528 MB
 	// Image id format: <hash>[:TAG]
 	
-	cmd = exec.Command("cd \"" + tempDirPath + "\"; /usr/bin/docker", "build",
+	cmd = exec.Command("/usr/bin/docker", "build",
     	"--file", dockerfileName, "--tag", imageName, tempDirPath)
 	
 	// Execute the command in the temporary directory.
