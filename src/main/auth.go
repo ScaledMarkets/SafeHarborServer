1cf5a43781557448bb890f91d5bff60437d88390
Clifford Berg <cliff@cliffberg.com>
2015-11-22 18:40:48 -0500

    Diagnostics.

--- src/main/auth.go
+++ src/main/auth.go
@@ -273,6 +273,12 @@ func (authSvc *AuthService) validateSessionId(sessionId string) *SessionToken {
 	
 	if credentials == nil {
 		fmt.Println("No credentials found for session id", sessionId)
+		fmt.Println("Sessions:")
+		for key, value := range authSvc.Sessions {
+			var valStr string
+			if value == nil { valStr = "nil" } else { valStr = "'" + value.UserId + "/" + value.Password + "'" }
+			fmt.Println("\t" + key + "='" + valStr + "'")
+		}
 		return nil
 	}
 	
