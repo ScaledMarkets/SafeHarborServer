3f481c9d8d281d8c3ceee6725c8a21a4e0e1faa4
Clifford Berg <cliff@cliffberg.com>
2016-01-17 19:30:54 -0500

    diag

--- src/safeharbor/server/InMemory.go
+++ src/safeharbor/server/InMemory.go
@@ -173,6 +173,7 @@ var _ PersistObj = &InMemPersistObj{}
 func (client *InMemClient) NewInMemPersistObj() (*InMemPersistObj, error) {
 	
 	fmt.Println("Creating persistent object")
+	debug.PrintStack()
 	
 	var id string
 	var err error
