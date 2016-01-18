d8ed7bf9dc8c6f7ac3496e70e6e494a9f3f7bb5c
Clifford Berg <cliff@cliffberg.com>
2016-01-18 13:16:06 -0500

    Added check for zero value

--- src/safeharbor/server/DBjson.go
+++ src/safeharbor/server/DBjson.go
@@ -32,6 +32,7 @@ func GetObject(target interface{}, json string) (string, interface{}, error) {
 	var methodName = "New" + typeName
 	var method = reflect.ValueOf(target).MethodByName(methodName)
 	if err != nil { return typeName, nil, err }
+	if ! method.IsValid() { return typeName, nil, errors.New("Method is unknown") }
 	
 	var argAr []reflect.Value
 	argAr, err = parseJSON(remainder)
