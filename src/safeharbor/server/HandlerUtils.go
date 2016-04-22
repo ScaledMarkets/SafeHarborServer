00c5ff2395ff0b13126a2bbc547ee3c6028bfabb
Clifford Berg <cliff@cliffberg.com>
2016-04-22 09:19:47 -0400

    diag

--- src/safeharbor/server/HandlerUtils.go
+++ src/safeharbor/server/HandlerUtils.go
@@ -330,7 +330,7 @@ func buildDockerfile(dbClient DBClient, dockerfile Dockerfile, sessionToken *api
 	var dockerBuildOutput *docker.DockerBuildOutput
 	dockerBuildOutput, err = docker.ParseBuildRESTOutput(outputStr)
 	fmt.Println("buildDockerfile: H")  // debug
-	fmt.Println("dockerBuildOutput=" + dockerBuildOutput.String()) // debug
+	fmt.Println("dockerBuildOutput=" + outputStr) // debug
 	fmt.Println("buildDockerfile: H1")  // debug
 	if err != nil { return nil, err }
 	var dockerImageId string = dockerBuildOutput.GetFinalDockerImageId()
