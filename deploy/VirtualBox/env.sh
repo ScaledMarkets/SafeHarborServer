ba6903b82c952f69ff9d50aaf9a84cb697b469d1
Cliff Berg <cliff@cliffberg.com>
2017-09-17 13:09:10 -0400

    misc

---
+++ deploy/VirtualBox/env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Env config for Centos running in VirtualBox.
+# Edit these values for your deployment:
+# Note: The DataVolMountPoint must be "shared" with the docker daemon. Use
+# the docker app configuration tool to do this.
+export DataVolMountPoint=/safeharbor/data  # this gets mapped to the container data directory.
+export registryUser=safeharbor
+export SafeHarborServerHost=127.0.0.1
+export SafeHarborPort=6000
+export RegistryPort=5000
+
+export TwistlockDir=/twistlock
+
+# Do not change:
+export PROJECTROOT=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )/../..
+. $PROJECTROOT/build/common/env.sh
+#export ScaledMarketsRegistryNamespace=scaledmarkets
+#export ScaledMarketsRegistryNamespace=500058573678.dkr.ecr.us-east-1.amazonaws.com
+#export SafeHarborImageName=$ScaledMarketsRegistryNamespace/safeharborserver
+export TARGET_ENV_CONFIGURED=yes
