cef961736b88da6e8635d9df6fad1c0f29984ecf
Cliff Berg <cliff@cliffberg.com>
2017-09-17 13:51:09 -0400

    misc

--- deploy/AWS/stop.sh
+++
@@ -1,5 +0,0 @@
-sudo docker stop safeharborserver
-sudo docker stop redis
-sudo docker stop registry
-sudo docker stop clair
-sudo docker stop postgres
--- deploy/AWS/undeploy.sh
+++
@@ -1,2 +0,0 @@
-rm -r registryauth
-rm -r registrydata
